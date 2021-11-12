db7c4121600d4c46dcfc08a19f9d25a16b714031
Alex-Verg <alexvergelyuk@gmail.com>
2021-11-12 09:54:48 +0200

    Make gradient_boosting function.

--- simple_iteration.py
+++ simple_iteration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def read_system():
@@ -28,16 +29,42 @@ def simple_iteration(A, B, eps=1e-5):
 
     x_1 = np.copy(nB)
     x_2 = nA @ x_1 + nB
-    print(nA)
     while np.linalg.norm(B - A @ x_2) > eps:
-        print(x_2)
+        #print(x_2)
         x_1 = x_2
         x_2 = nA @ x_1 + nB
 
     return x_2
 
 
+def gradient_boosting(A, B, eps=1e-5):
+    x_2 = np.copy(B)
+    x_1 = np.zeros_like(B)
+    r = np.zeros_like(B)
+    n = len(B)
+    while np.linalg.norm(B - A @ x_2) > eps:
+        x_1 = x_2
+        for i in range(n):
+            sum1 = 0
+            sum2 = 0
+            for j in range(n):
+                for k in range(n):
+                    sum1 += A[i, j] * A[j, k] * x_1[k]
+                sum2 += A[i, j] * B[j, 0]
+            r[i, 0] = - sum1 + sum2
+        alpha = (np.sum(r ** 2)) / np.sum((A @ r) ** 2)
+        x_2 = x_1 + alpha * r
+
+    return x_2
+
+
 if __name__ == "__main__":
     A, B = read_system()
+    st = time.time()
     x = simple_iteration(A, B)
-    print(A @ x)
+    fin = time.time()
+    print(x, fin-st)
+    st = time.time()
+    x2 = gradient_boosting(A, B)
+    fin = time.time()
+    print(x2, fin-st)
